ada7a2cf85b074b1bdff357c74472c670996e3ec
Paul Hirschi <hirschi.pt@gmail.com>
2020-04-24 10:30:24 -0600

    solutions in scala for 1.1, improvements to lisp fns

--- book/1.1/solution.lisp
+++ book/1.1/solution.lisp
@@ -1,52 +1,34 @@
-(defvar the_list '(1 2 3 4 5)) ;; => (120 60 40 30 24)
+(defvar *the_list* '(1 2 3 4 5)) ;; => (120 60 40 30 24)
 
 ;;; Get the product list using division
 ;;; Find product of entire list using a reducing scheme
-;;; then divide by index value of list and append resulting value
+;;; Map through the list, divide product by element at index
 (defun get_product_list_division (list)
-    (let ((product) (product_list))
+    (let ((product))
         ;; get product of entire list
-        (setf product (apply '* list))
-        (dolist (x list)
-            (let ((new_product))
-                ;; device product by index value
-                (setf new_product (/ product x))
-                ;; append resulting product to the final product_list
-                (setf product_list (append product_list (list new_product)))))
-        ;; return the product_list
-        product_list))
+        (setq product (apply '* list))
+        (mapcar #'(lambda (x) (/ product x)) list)))
 
 
-;;; Shot at solving the problem using recursion.
-;;; Initial function takes the list (li), prefix (pref), and accumulator (acc)
-;;; If the list is empty, return the accumulator
-;;; Each pass create a new prefix by appending the first item of the 
-;;; list to pref. Reduce multiply the prefix and suffix, then multiply together.
-;;; Append these values to accumulator
-;;; Recurse by passing in (rest li) prefix accumu
-;;; The initial call passes in the whole list, and nil for pref and acc
+;;; Tail recursive function that multiplies suffix elements (li) and
+;;; prefix elements (pref) and keeps track with accumulator (acc)
 (defun get_product_list_rec (li pref acc)
     (cond
-        ;; if li is empty, return acc
         ((null li) acc)
-        ;; define new vars
         (t (let ((suffix) (prefix) (prod) (accum))
-            ;; prefix is pre with first of li
-            (setf prefix (append pref (list (first li))))
-            ;; suffix is reduce of * of rest of li
-            (setf suffix (apply '* (rest li)))
-            ;; prod is * of suffix (tail end of list) with reduce of * over prefix butlast 1 (excluding current index)
-            (setf prod (* suffix (apply '* (butlast prefix 1))))
-            ;; append prod to acc for new accum
-            (setf accum (append acc (list prod)))
+            (setq
+                ;; prefix is pre with first of li
+                prefix (append pref (list (first li)))
+                ;; suffix is reduce of * over rest of li
+                suffix (apply '* (rest li))
+                ;; prod is * of suffix (tail end of list) with reduce of * over prefix butlast 1 (excluding current index)
+                prod (* suffix (apply '* (butlast prefix 1)))
+                ;; append prod to acc for new accum
+                accum (append acc (list prod)))
             ;; make recursive call
             (get_product_list_rec (rest li) prefix accum)))))
 
-;;; An iterative approach.
-(defun get_product_list_iteration (list)
-    ;; TODO)
 
-
-(print (get_product_list_division the_list))
-(print (get_product_list_rec the_list nil nil))
+(print (get_product_list_division *the_list*) )
+(print (get_product_list_rec *the_list* nil nil) )
 
---
+++ book/1.1/solution.scala
@@ -0,0 +1,23 @@
+val theList: List[Int] = List(1, 2, 3, 4, 5) // 
+
+// Simple reduction and map
+def getProductListDivision(list: List[Int]): List[Int] = {
+  val product = list.product // list.reduceLeft(_ * _) list.foldLeft(1)(_ * _)
+  list map (product / _)
+}
+
+// Tail recursive function that multiplies prefix elements to suffix elements
+// and keeps track with accumulator
+@scala.annotation.tailrec
+def getProductListRec(rest: List[Int], prefix: List[Int], acc: List[Int]): List[Int] = rest match {
+  case Nil => acc
+  case x :: xs => {
+    val product = prefix.product * xs.product
+    val newPrefix = prefix :+ x
+    val newAccum = acc :+ product
+    getProductListRec(xs, newPrefix, newAccum)
+  }
+}
+
+println( getProductListDivision(theList) )
+println( getProductListRec(theList, Nil, Nil) )
